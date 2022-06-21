3b027e119c54ff977461a71b7d14b08ef8ff7996
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-21 23:02:43 +0700

    added login header and email input field

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -1,11 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/theme.dart';
 
 class SignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Login',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 24, fontWeight: semiBold),
+            ),
+            SizedBox(height: 2),
+            Text(
+              'Sign in to your account',
+              style: subtitleText,
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget emailInput() {
+      return Container(
+        margin: EdgeInsets.only(top: 70),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Email Address',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            ),
+            SizedBox(height: 12),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: bgColor2,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/img/icon_email.png',
+                      width: 17,
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                        child: TextFormField(
+                      style: primaryTextStyle,
+                      decoration: InputDecoration.collapsed(
+                          hintText: 'Enter your email',
+                          hintStyle: subtitleText),
+                    ))
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-        body: Center(
-      child: Text('Sign In Page'),
-    ));
+        backgroundColor: bgColor1,
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                header(),
+                emailInput(),
+              ],
+            ),
+          ),
+        ));
   }
 }
