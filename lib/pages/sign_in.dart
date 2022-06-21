bf1600d35a457d91f1a054e2b12a919ec6f4c3d8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-21 23:15:16 +0700

    added password input field and sign button

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -68,7 +68,91 @@ class SignIn extends StatelessWidget {
       );
     }
 
+    Widget passwordInput() {
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Password',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            ),
+            SizedBox(height: 12),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: bgColor2,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/img/icon_password.png',
+                      width: 17,
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                        child: TextFormField(
+                      style: primaryTextStyle,
+                      obscureText: true,
+                      decoration: InputDecoration.collapsed(
+                          hintText: 'Your password', hintStyle: subtitleText),
+                    ))
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget signInBtn() {
+      return Container(
+        width: double.infinity,
+        height: 50,
+        margin: EdgeInsets.only(top: 30),
+        child: TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+                backgroundColor: primaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12))),
+            child: Text(
+              'Sign In',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            )),
+      );
+    }
+
+    Widget footer() {
+      return Container(
+        margin: EdgeInsets.only(bottom: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Don't have an account? ",
+              style: subtitleText.copyWith(
+                fontSize: 12,
+              ),
+            ),
+            Text(
+              "Sign Up",
+              style: purpleTextStyle.copyWith(fontSize: 12, fontWeight: medium),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: bgColor1,
         body: SafeArea(
           child: Container(
@@ -78,6 +162,10 @@ class SignIn extends StatelessWidget {
               children: [
                 header(),
                 emailInput(),
+                passwordInput(),
+                signInBtn(),
+                Spacer(),
+                footer()
               ],
             ),
           ),
