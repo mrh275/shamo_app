a7403175d46cb8b3b467550ac57a09a84cc0c5e9
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-22 23:13:53 +0700

    added route for homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shamo_app/home/main_page.dart';
 import 'package:shamo_app/pages/sign_up.dart';
 import 'package:shamo_app/pages/splash_screen.dart';
 import 'package:shamo_app/pages/sign_in.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => SplashScreen(),
         '/sign-in': (context) => SignIn(),
         '/sign-up': (context) => SignUp(),
+        '/home': (context) => MainPage(),
       },
     );
   }
