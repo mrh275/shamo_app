121c027aee8b4c875d2201d3c1fab34b25b10010
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-22 23:25:53 +0700

    created bottom navbar

--- lib/home/main_page.dart
+++ lib/home/main_page.dart
@@ -1,9 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/theme.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget cartButton() {
+      return FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: secondaryColor,
+        child: Image.asset(
+          'assets/img/icon_cart.png',
+          width: 20,
+        ),
+      );
+    }
+
+    Widget customBottomNav() {
+      return ClipRRect(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(30),
+        ),
+        child: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 10,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+            backgroundColor: bgColor4,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/img/icon_home.png',
+                  width: 21,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/img/icon_chat.png',
+                  width: 20,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/img/icon_favorite.png',
+                  width: 20,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/img/icon_profile.png',
+                  width: 18,
+                ),
+                label: '',
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
+      backgroundColor: bgColor1,
+      floatingActionButton: cartButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: customBottomNav(),
       body: Center(child: Text('Main Page')),
     );
   }
