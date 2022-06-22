74583943831c428551d6118043dfd0900561f1ed
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-22 23:58:21 +0700

    created header

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/theme.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Homepage'),
-      ),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultMargin, left: defaultMargin, right: defaultMargin),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hello, Dani',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 24, fontWeight: semiBold),
+                  ),
+                  Text(
+                    '@ramdani275',
+                    style: subtitleText.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: AssetImage('assets/img/user_profile.png')),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
