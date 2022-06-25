21de41041f3d40fb8efe82422d034c2e7d188105
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-25 22:53:09 +0700

    added product card

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shamo_app/theme.dart';
+import 'package:shamo_app/widgets/product_cart.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -142,10 +143,50 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget popularProductsTitle() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Text(
+          'Popular Products',
+          style: primaryTextStyle.copyWith(
+            fontSize: 22,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    Widget popularProducts() {
+      return Container(
+        margin: EdgeInsets.only(top: 14),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(children: [
+            SizedBox(
+              width: defaultMargin,
+            ),
+            Row(
+              children: [
+                ProductCard(),
+                ProductCard(),
+                ProductCard(),
+              ],
+            ),
+          ]),
+        ),
+      );
+    }
+
     return ListView(
       children: [
         header(),
         categories(),
+        popularProductsTitle(),
+        popularProducts(),
       ],
     );
   }
