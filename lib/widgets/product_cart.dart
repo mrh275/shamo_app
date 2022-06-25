57ccb22f9bdcdcea808d16ef0ff1dd1f9de145c4
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-25 22:52:57 +0700

    create product card

---
+++ lib/widgets/product_cart.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:shamo_app/theme.dart';
+
+class ProductCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 215,
+      height: 278,
+      margin: EdgeInsets.only(
+        right: defaultMargin,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: bgShoes,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Image.asset(
+            'assets/img/image_shoes.png',
+            width: 215,
+            height: 150,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hiking',
+                  style: secondaryTextStyle.copyWith(
+                    fontSize: 12,
+                  ),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'COURT VISION 2.0',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: bold,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  '\$58,67',
+                  style: priceTextStyle.copyWith(
+                    fontSize: 14,
+                    fontWeight: medium,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
