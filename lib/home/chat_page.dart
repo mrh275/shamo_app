782797dda045eea29c74289021a49f0928a4baa5
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-26 16:50:39 +0700

    modified empty chat page

--- lib/home/chat_page.dart
+++ lib/home/chat_page.dart
@@ -1,12 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/theme.dart';
 
 class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Chat Page'),
-      ),
+    Widget header() {
+      return AppBar(
+        backgroundColor: bgColor1,
+        centerTitle: true,
+        title: Text(
+          'Message Support',
+          style: primaryTextStyle.copyWith(fontSize: 18, fontWeight: medium),
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          color: bgColor3,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/img/icon_headset.png',
+                width: 80,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Opps, no message yet?',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                'You\'ve never done any transactions.',
+                style: secondaryTextStyle,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 44,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 10,
+                    ),
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    'Explore Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
